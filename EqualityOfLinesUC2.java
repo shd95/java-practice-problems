15e5f833a05d8fa4da5cae529199cc8c16395298
shd95 <sriharshadevale@gmail.com>
2021-04-15 16:41:42 +0530

    Checking the length of both lines which are made by cartesian co-ordinates

---
+++ EqualityOfLinesUC2.java
@@ -0,0 +1,28 @@
+public class EqualityOfLinesUC2 {
+
+public static void main(String[] args) {
+// cartesian co-ordinates of first line
+int X1=2;
+int X2=4;
+int Y1=3;
+int Y2=5;
+//cartsian co-ordinates of second line
+int A1=3;
+int A2=5;
+int B1=4;
+int B2=8;
+// computation
+double lengthLine1 = Math.pow((X2-X1),2) + Math.pow((Y2-Y1),2);
+double lengthLine2 = Math.pow((A2-A1),2) + Math.pow((B2-B1),2);
+
+System.out.println("First line length is: " + lengthLine1);
+System.out.println("Second line length is: " + lengthLine2);
+
+if (lengthLine1 <  lengthLine2)
+System.out.println("first line is longer");
+else if (lengthLine2 < lengthLine1)
+System.out.println("second line is longer");
+else
+System.out.println("Both lines are equal");
+	}
+}
