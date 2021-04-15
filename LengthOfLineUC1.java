5a862eaec494886da5b667d3275cdc25d19449ee
shd95 <sriharshadevale@gmail.com>
2021-04-15 15:41:06 +0530

    Finding the length of the line made by cartesian co-ordinates

---
+++ LengthOfLineUC1.java
@@ -0,0 +1,13 @@
+public class LengthOfLineUC1 {
+
+public static void main(String[] args) {
+// Constants
+double X1 = 2;
+double Y1 = 3;
+double X2 = 4;
+double Y2 = 7;
+
+double lengthCheck = Math.pow((X2 - X1),2) + Math.pow((Y2 - Y1),2);
+System.out.println("Length of a line is: " + lengthCheck);
+	}
+}
